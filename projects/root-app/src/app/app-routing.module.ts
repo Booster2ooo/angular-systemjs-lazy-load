64e1976a6be8df47a9ebc2c6b8f4d705dfa52dc2
Booster2ooo <the._.truth@hotmail.fr>
2021-02-26 18:07:14 +0100

    Avoided window monkey patching
    - used proper systemjs methodology to provide angular modules, closing #1

--- projects/root-app/src/app/app-routing.module.ts
+++ projects/root-app/src/app/app-routing.module.ts
@@ -5,47 +5,51 @@ import * as angularCore from '@angular/core';
 import * as angularCommon from '@angular/common';
 import * as angularRouter from '@angular/router';
 
-(window as any).ng = {
- core: angularCore,
- common: angularCommon,
- router: angularRouter
-};
+const System = (window as any).System;
+System.set('app:@angular/core', angularCore);
+System.set('app:@angular/common', angularCommon);
+System.set('app:@angular/router', angularRouter);
 
-var module = 'a-remote-module';
+/*(window as any).ng = {
+	core: angularCore,
+	common: angularCommon,
+	router: angularRouter
+};*/
+
 
 const modules = [
-	{ path: 'remote', moduleId: 'a-remote-module', moduleClass: 'ARemoteModuleModule'},
-	{ path: 'scoped-remote', moduleId: '@a-scope/another-remote-module', moduleClass: 'AnotherRemoteModuleModule'},
+	{ path: 'remote', moduleId: 'a-remote-module', moduleClass: 'ARemoteModuleModule' },
+	{ path: 'scoped-remote', moduleId: '@a-scope/another-remote-module', moduleClass: 'AnotherRemoteModuleModule' },
 ];
 
 const appRoutes: Routes = [
-  ...modules.map((module) => ({
-	  path: module.path,
-	  loadChildren: () => {
-		return (window as any).System.import(module.moduleId).then((m: any) => {
-		  if (module.moduleId.indexOf('@') === 0) {
-			  return m[module.moduleId.split('/')[1]][module.moduleClass];
-		  }
-		  return m[module.moduleClass];
-		});
-	  }
-  })),
-  { 
-    path: '', 
-    redirectTo: '', 
-    pathMatch: 'full' 
-  }
+	...modules.map((module) => ({
+		path: module.path,
+		loadChildren: () => {
+			return System.import(module.moduleId).then((m: any) => {
+				if (module.moduleId.indexOf('@') === 0) {
+					return m[module.moduleId.split('/')[1]][module.moduleClass];
+				}
+				return m[module.moduleClass];
+			});
+		}
+	})),
+	{
+		path: '',
+		redirectTo: '',
+		pathMatch: 'full'
+	}
 ];
 
 @NgModule({
-  imports: [
-    RouterModule.forRoot(
-      appRoutes
-      //,{ enableTracing: true } // <-- debugging purposes only
-    )
-  ],
-  exports: [
-    RouterModule
-  ]
+	imports: [
+		RouterModule.forRoot(
+			appRoutes
+			//,{ enableTracing: true } // <-- debugging purposes only
+		)
+	],
+	exports: [
+		RouterModule
+	]
 })
 export class AppRoutingModule { }
